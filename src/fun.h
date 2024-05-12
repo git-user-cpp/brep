2eb28d85118f6a12d61afef755fc95753f110bde
git-user-cpp <akushyk799@proton.me>
2024-05-12 14:41:25 +0300

    v1.0.0 license

--- src/fun.h
+++ src/fun.h
@@ -1,5 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * binary representator written in C
+ * Copyright (C) 2024  Andrew Kushyk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-void print_binary(unsigned long long n, int i);
-void validate_scan(int args);
+#ifndef FUN_H_SENTRY
+#define FUN_H_SENTRY
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    void print_binary(unsigned long long n, int i);
+    void validate_scan(int args);
+
+#endif
