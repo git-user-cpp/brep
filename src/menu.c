5dbb19c96dba05d010666d0ea4e489d7f7a3541c
destatr <destator210@gmail.com>
2024-05-12 22:20:12 +0200

    Replaced the if() construct with switch...case in the menu().

--- src/menu.c
+++ src/menu.c
@@ -34,54 +34,65 @@ int initial_choice(void) {
     return choice;
 }
 
-void menu(int choice) {
+void menu(int choice)
+{
     int scf_chk;
-
-    if(choice == 1) {
-        int buf;
-        char num;
-        printf("Enter a char: ");
-        scf_chk = scanf("%d", &buf);
-        validate_scan(scf_chk);
-        num = (char) buf;
-        print_binary(num, sizeof(num) * 8);
-    } else if(choice == 2) {
-        short num;
-        printf("Enter a short: ");
-        scf_chk = scanf("%hd", &num);
-        validate_scan(scf_chk);
-        print_binary(num, sizeof(num) * 8);
-    } else if(choice == 3) {
-        int num;
-        printf("Enter an integer: ");
-        scf_chk = scanf("%d", &num);
-        validate_scan(scf_chk);
-        print_binary(num, sizeof(num) * 8);
-    } else if(choice == 4) {
-        long num;
-        printf("Enter a long: ");
-        scf_chk = scanf("%ld", &num);
-        validate_scan(scf_chk);
-        print_binary(num, sizeof(num) * 8);
-    } else if(choice == 5) {
-        long long num;
-        printf("Enter a long long: ");
-        scf_chk = scanf("%lld", &num);
-        validate_scan(scf_chk);
-        print_binary(num, sizeof(num) * 8);
-    } else if(choice == 6) {
-        float num;
-        printf("Enter a float: ");
-        scf_chk = scanf("%f", &num);
-        validate_scan(scf_chk);
-        print_binary(*((unsigned int*)&num), sizeof(num) * 8);
-    } else if(choice == 7) {
-        double num;
-        printf("Enter a double: ");
-        scf_chk = scanf("%lf", &num);
-        validate_scan(scf_chk);
-        print_binary(*((unsigned long long*)&num), sizeof(num) * 8);
-    } else {
-        printf("Invalid choice.\n");
+    switch (choice)
+    {
+        case 1:
+            int buf;
+            char num;
+            printf("Enter a char: ");
+            scf_chk = scanf("%d", &buf);
+            validate_scan(scf_chk);
+            num = (char) buf;
+            print_binary(num, sizeof(num) * 8);
+            break;
+        case 2:
+            short short_num;
+            printf("Enter a short: ");
+            scf_chk = scanf("%hd", &short_num);
+            validate_scan(scf_chk);
+            print_binary(short_num, sizeof(short_num) * 8);
+            break;
+        case 3:
+            int int_num;
+            printf("Enter an integer: ");
+            scf_chk = scanf("%d", &int_num);
+            validate_scan(scf_chk);
+            print_binary(int_num, sizeof(int_num) * 8);
+            break;
+        case 4:
+            long long_num;
+            printf("Enter a long: ");
+            scf_chk = scanf("%ld", &long_num);
+            validate_scan(scf_chk);
+            print_binary(long_num, sizeof(long_num) * 8);
+            break;
+        case 5:
+            long long longl_num;
+            printf("Enter a long long: ");
+            scf_chk = scanf("%lld", &longl_num);
+            validate_scan(scf_chk);
+            print_binary(longl_num, sizeof(longl_num) * 8);
+            break;
+        case 6:
+            float float_num;
+            printf("Enter a float: ");
+            scf_chk = scanf("%f", &float_num);
+            validate_scan(scf_chk);
+            print_binary(*((unsigned int*)&float_num), sizeof(float_num) * 8);
+            break;
+        case 7:
+            double double_num;
+            printf("Enter a double: ");
+            scf_chk = scanf("%lf", &double_num);
+            validate_scan(scf_chk);
+            print_binary(*((unsigned long long*)&double_num), sizeof(double_num) * 8);
+            break;
+        default:
+            printf("Invalid choice.\n");
+            break;
     }
+
 }
